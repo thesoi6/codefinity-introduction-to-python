32aeec19d46f4d7e814f4935fa1998576bd6b14e
thesoi6 <>
2025-11-11 20:09:55 +0000

    Variables

---
+++ variables_and_types/variables/main.py
@@ -0,0 +1,15 @@
+item_name = "Banana"
+item_price = 3.99
+item_stock_quantity = 120
+in_stock = True
+
+
+# Testing
+print("Item: ", item_name)
+print("Price per unit: $", item_price)
+print("quantity in stock: ", item_stock_quantity)
+print("In stock: ", in_stock)
+
+# Reassigning the variable
+item_name = "Eggs"
+print("item:", item_name)
